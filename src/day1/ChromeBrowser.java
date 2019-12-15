c93e6d700c9db45c745a535e59ba8a08aa603f59
Markoidze <manucharmarkoidze@gmail.com>
2019-12-15 13:59:42 -0500

    Work

--- src/day1/ChromeBrowser.java
+++ src/day1/ChromeBrowser.java
@@ -1,12 +1,18 @@
 package day1;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ChromeBrowser {
     public static void main(String[] args) {
-        System.setProperty( "webdriver.chrome.driver", "D:\\TechnoStudy\\Selenium\\ChromeDriver\\chromedriver.exe" );
+        System.setProperty( "webdriver.chrome.driver", "C:\\Users\\manuchar.markoidze.PCSST\\Desktop\\MySelenium\\Chrom driver\\chromedriver.exe" );
         WebDriver driver = new ChromeDriver();
         driver.get( "https://www.techno.study/" );
+        WebElement text = driver.findElement( By.name( "text" ) );
+        text.sendKeys( "This is my text" );
+        WebElement webElement = driver.findElement( By.name( "password" ) );
+        webElement.sendKeys( "Password" );
     }
 }
--- src/day3/ChromeBrowser.java
+++ src/day3/ChromeBrowser.java
@@ -1,12 +1,27 @@
 package day3;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class ChromeBrowser {
     public static void main(String[] args) {
-        System.setProperty( "webdriver.chrome.driver", "D:\\TechnoStudy\\Selenium\\ChromeDriver\\chromedriver.exe" );
+        System.setProperty( "webdriver.chrome.driver", "C:\\Users\\manuchar.markoidze.PCSST\\Desktop\\MySelenium\\Chrom driver\\chromedriver.exe" );
         WebDriver driver = new ChromeDriver();
-        driver.get( "https://www.techno.study/" );
+        driver.get( "file:///C:/Users/manuchar.markoidze.PCSST/Desktop/MySelenium/selenium/selenium/selenium/src/day3/resources/form.html" );
+        WebElement text = driver.findElement( By.name( "text" ) );
+        text.sendKeys( "This is my text" );
+        WebElement webElement = driver.findElement( By.name( "password" ) );
+        webElement.sendKeys( "Password" );
+        WebElement textarea = driver.findElement( By.name( "textarea" ) );
+        textarea.sendKeys( "Some text Area words" );
+        WebElement checkbox = driver.findElement( By.name( "checkbox" ) );
+        checkbox.click();
+        List<WebElement> radio = driver.findElements( By.name( "radio" ) );
+        WebElement secondElementOfRadio = radio.get( 1 );// [0, 1, 2]
+        secondElementOfRadio.click();
     }
 }
